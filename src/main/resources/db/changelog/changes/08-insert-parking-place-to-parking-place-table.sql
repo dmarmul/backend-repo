74222ff3fff42042728ee2d5f0f1d4e7662ee92c
Dmitry <dmarmul228@gmail.com>
2024-12-19 21:24:54 +0200

    added new neighborhood information to db

--- src/main/resources/db/changelog/changes/08-insert-parking-place-to-parking-place-table.sql
+++ src/main/resources/db/changelog/changes/08-insert-parking-place-to-parking-place-table.sql
@@ -1,10 +1,10 @@
 INSERT INTO parking_place (id, neighborhood_id, parking_place)
 VALUES
     (1, 1, 'Q-Park de Bijenkorf'),
-    (2, 2, 'There is no parking place yet('),
+    (2, 2, 'Parking Museum Quarter'),
     (3, 3, 'Parkeren bij De Wallen'),
     (4, 4, 'Jordaan- Amsterdam'),
-    (5, 5, 'There is no parking place yet('),
+    (5, 5, 'Park Nieuwendijk'),
     (6, 6, 'Jordaan- Amsterdam'),
     (7, 7, 'Parking Plantage'),
     (8, 8, 'Parking de Pijp'),
@@ -14,6 +14,6 @@ VALUES
     (12, 12, 'Q-Park Marnixstraat'),
     (13, 13, 'Q-Park Centrum Oost'),
     (14, 14, 'Amsterdam Sloterdijk'),
-    (15, 15, 'There is no parking place yet('),
-    (16, 16, 'There is no parking place yet('),
-    (17, 17, 'There is no parking place yet(');
+    (15, 15, 'Parking Rivierenbuurt'),
+    (16, 16, 'Amsterdam-Noord'),
+    (17, 17, 'Parking Westpoort Amsterdam');
